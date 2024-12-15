4c15328a42849c837f12d5acae97163b79d851b4
ngongocthang <ngongocthang18082004@gmail.com>
2024-12-15 18:38:19 +0700

    fix npm notifi

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,6 +16,18 @@ const Navbar = () => {
   const wsRef = useRef(null);
 
   useEffect(() => {
+    // Kiểm tra token khi component được khởi tạo
+    const token = localStorage.getItem("token");
+    if (token) {
+      // Nếu có token, bạn có thể lấy thông tin người dùng từ localStorage
+      const userData = JSON.parse(localStorage.getItem("user"));
+      if (userData) {
+        setUser(userData);
+      }
+    }
+  }, [setUser]);
+
+  useEffect(() => {
     if (user) {
       // Khởi tạo WebSocket
       const wsProtocol =
@@ -28,7 +40,6 @@ const Navbar = () => {
       wsRef.current = ws;
 
       ws.onopen = () => {
-        // console.log('WebSocket connection established');
         ws.send(JSON.stringify({ user_id: user.id }));
       };
 
@@ -63,7 +74,6 @@ const Navbar = () => {
       wsRef.current.send(
         JSON.stringify({ user_id: user.id, action: "update" })
       );
-      // console.log('Request sent to update notifications');
     }
   };
 
@@ -85,7 +95,6 @@ const Navbar = () => {
 
   const handleNotificationClick = () => {
     navigate("/notifications");
-    // Cập nhật trạng thái thông báo (nếu cần)
   };
 
   return (
